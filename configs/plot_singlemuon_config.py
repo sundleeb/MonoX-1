a0bdaa86e3ec16c089057bbb18cc516531e4f1d8
nick <n.c.k.wardle@googlemail.com>
2015-02-08 17:00:50 +0100

    Wrong template used

--- configs/plot_singlemuon_config.py
+++ configs/plot_singlemuon_config.py
@@ -8,8 +8,8 @@ signals = {
 key_order = ["QCD","Z#rightarrow ll","Dibosons","Top","W#rightarrow#mu#nu"]
 
 backgrounds = { 
-		"Top":			  [["singlemuon_top"],			r.kRed+1,   0]
-		,"Dibosons":		  [["singlemuon_dibosons"],		r.kGray,   0]
+		"Top":			  [["singlemuon_top"],			r.kRed+1,   0,1.12]
+		,"Dibosons":		  [["singlemuon_dibosons"],		r.kGray,    0,1.12]
 		,"W#rightarrow#mu#nu":	  [["photon_dimuon_combined_model.root:W$DIRECTORY/WJets_singlemuon_wjets_combined_model"],	r.kAzure-3,  0]
 		,"QCD":	  		  [["singlemuon_qcd"],			r.kRed+2,   0]
 		,"Z#rightarrow ll":	  [["photon_dimuon_combined_model.root:$DIRECTORY/ZJets_singlemuon_zll_combined_model"],	r.kGreen+3,  0]
--- configs/plot_singlemuon_config_jpt.py
+++ configs/plot_singlemuon_config_jpt.py
@@ -8,8 +8,8 @@ signals = {
 key_order = ["QCD","Z#rightarrow ll","Dibosons","Top","W#rightarrow#mu#nu"]
 
 backgrounds = { 
-		"Top":			  [["singlemuon_top$VAR"],		r.kRed+1,   0]
-		,"Dibosons":		  [["singlemuon_dibosons$VAR"],		r.kGray,   0]
+		"Top":			  [["singlemuon_top$VAR"],		r.kRed+1,   0,1.12]
+		,"Dibosons":		  [["singlemuon_dibosons$VAR"],		r.kGray,   0,1.12]
 		,"W#rightarrow#mu#nu":	  [["photon_dimuon_combined_model.root:W$DIRECTORY/WJets_singlemuon_wjets_combined_model$VAR"],		r.kAzure-3,  0]
 		,"QCD":	  		  [["singlemuon_qcd$VAR"],		r.kRed+2,   0]
 		,"Z#rightarrow ll":	  [["photon_dimuon_combined_model.root:$DIRECTORY/ZJets_singlemuon_zll_combined_model$VAR"],		r.kGreen+3,  0]
